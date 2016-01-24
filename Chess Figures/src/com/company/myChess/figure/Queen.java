7f0f51ebb48ff1111e27882920b0737407a5de75
ValyawkaA <nastia31127@mail.ru>
2016-01-24 21:00:34 +1000

    Begining

---
+++ Chess Figures/src/com/company/myChess/figure/Queen.java
@@ -0,0 +1,26 @@
+package com.company.myChess.figure;
+
+import com.company.myChess.Constants;
+import com.company.myChess.FigureType;
+import com.company.myChess.Move;
+
+/**
+ * Created by Диана on 13.01.2016.
+ */
+public class Queen extends Figure implements Move {
+
+    public Queen(int x, int y, FigureType figureType) {
+        super(x, y, figureType);
+    }
+
+    @Override
+    public void move(int x, int y) throws Exception {
+        if ((this.x == x && this.y != y) || (this.y == y && this.x != x) || (Math.abs(this.x - x) == Math.abs(this.y - y))) {
+            this.x = x;
+            this.y = y;
+        } else {
+            throw new Exception(Constants.ILLEGAL_MOVE_MSG);
+        }
+
+    }
+}
